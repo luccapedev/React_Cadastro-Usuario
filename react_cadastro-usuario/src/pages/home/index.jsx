6df12eefd750bd40e4ca7fa3e95f0f3b8e6a4df2
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-04-24 14:52:35 -0300

    Criando o cadastro

--- react_cadastro-usuario/src/pages/home/index.jsx
+++ react_cadastro-usuario/src/pages/home/index.jsx
@@ -1,25 +1,46 @@
 import './style.css'
+import Trash from '../../assets/trash.svg'
 
 function Home() {
 
-  return (
+  const users = [
+    {
+      id: '1312313',
+      name: 'Rodolfo',
+      age: 33,
+      email: 'rod@email.com',
+    },
+    {
+      id: '4564333',
+      name: 'Aline',
+      age: 28,
+      email: 'rodrod@email.com',
+    },
+  ]
 
+  return (
     <div className='container'>
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input name='nome' type='text'/>
-        <input name='idade' type='number'/>
-        <input name='email' type='email'/>
+        <input name='nome' type='text' />
+        <input name='idade' type='number' />
+        <input name='email' type='email' />
         <button type='button'>Cadastrar</button>
       </form>
-      <div>
-        <div>
-          <p>Nome: </p>
-          <p>Idade: </p>
-          <p>Email: </p>
-          <p></p>
+
+      {users.map(user => (
+        <div key={user.id}>
+          <div>
+            <p>Nome: {user.name}</p>
+            <p>Idade: {user.age}</p>
+            <p>Email: {user.email}</p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
         </div>
-      </div>
+      ))}
+
     </div>
   )
 }
